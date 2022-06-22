e1a40fde50d30f165c3aa79ff86075aa711afeed
Ibragim Bakaev <98455005+raininsnovv@users.noreply.github.com>
2022-06-22 21:05:47 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              width: 100,
+              color: Colors.red,
+              child: Text('Container 1'),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.blueGrey,
+                )
+              ],
+            ),
+            Container(
+              width: 100,
+              color: Colors.blue,
+              child: Text('Container 3'),
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+}
